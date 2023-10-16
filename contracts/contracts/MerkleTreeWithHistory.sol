90786e13f7c2a14d997aab1073251e8540edc3d2
Nobuhito Kurose <pudding.heart.hd@gmail.com>
2023-10-16 19:44:34 +0900

    replace MiMC hash with keccak256

--- contracts/contracts/MerkleTreeWithHistory.sol
+++ contracts/contracts/MerkleTreeWithHistory.sol
@@ -1,49 +1,24 @@
-// https://tornado.cash
-/*
- * d888888P                                           dP              a88888b.                   dP
- *    88                                              88             d8'   `88                   88
- *    88    .d8888b. 88d888b. 88d888b. .d8888b. .d888b88 .d8888b.    88        .d8888b. .d8888b. 88d888b.
- *    88    88'  `88 88'  `88 88'  `88 88'  `88 88'  `88 88'  `88    88        88'  `88 Y8ooooo. 88'  `88
- *    88    88.  .88 88       88    88 88.  .88 88.  .88 88.  .88 dP Y8.   .88 88.  .88       88 88    88
- *    dP    `88888P' dP       dP    dP `88888P8 `88888P8 `88888P' 88  Y88888P' `88888P8 `88888P' dP    dP
- * ooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooo
- */
-
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.7.0;
-
-interface IHasher {
-    function MiMCSponge(
-        uint256 in_xL,
-        uint256 in_xR
-    ) external pure returns (uint256 xL, uint256 xR);
-}
+pragma solidity ^0.8.9;
 
 contract MerkleTreeWithHistory {
     uint256 public constant FIELD_SIZE =
         21888242871839275222246405745257275088548364400416034343698204186575808495617;
     uint256 public constant ZERO_VALUE =
         21663839004416932945382355908790599225266501822907911457504978515578255421292; // = keccak256("tornado") % FIELD_SIZE
-    IHasher public immutable hasher;
 
     uint32 public levels;
 
-    // the following variables are made public for easier testing and debugging and
-    // are not supposed to be accessed in regular code
-
-    // filledSubtrees and roots could be bytes32[size], but using mappings makes it cheaper because
-    // it removes index range check on every interaction
     mapping(uint256 => bytes32) public filledSubtrees;
     mapping(uint256 => bytes32) public roots;
     uint32 public constant ROOT_HISTORY_SIZE = 30;
     uint32 public currentRootIndex = 0;
     uint32 public nextIndex = 0;
 
-    constructor(uint32 _levels, IHasher _hasher) {
+    constructor(uint32 _levels) {
         require(_levels > 0, "_levels should be greater than zero");
         require(_levels < 32, "_levels should be less than 32");
         levels = _levels;
-        hasher = _hasher;
 
         for (uint32 i = 0; i < _levels; i++) {
             filledSubtrees[i] = zeros(i);
@@ -52,11 +27,7 @@ contract MerkleTreeWithHistory {
         roots[0] = zeros(_levels - 1);
     }
 
-    /**
-    @dev Hash 2 tree leaves, returns MiMC(_left, _right)
-  */
     function hashLeftRight(
-        IHasher _hasher,
         bytes32 _left,
         bytes32 _right
     ) public pure returns (bytes32) {
@@ -68,12 +39,7 @@ contract MerkleTreeWithHistory {
             uint256(_right) < FIELD_SIZE,
             "_right should be inside the field"
         );
-        uint256 R = uint256(_left);
-        uint256 C = 0;
-        (R, C) = _hasher.MiMCSponge(R, C);
-        R = addmod(R, uint256(_right), FIELD_SIZE);
-        (R, C) = _hasher.MiMCSponge(R, C);
-        return bytes32(R);
+        return keccak256(abi.encodePacked(_left, _right));
     }
 
     function _insert(bytes32 _leaf) internal returns (uint32 index) {
@@ -96,7 +62,7 @@ contract MerkleTreeWithHistory {
                 left = filledSubtrees[i];
                 right = currentLevelHash;
             }
-            currentLevelHash = hashLeftRight(hasher, left, right);
+            currentLevelHash = hashLeftRight(left, right);
             currentIndex /= 2;
         }
 
@@ -107,9 +73,7 @@ contract MerkleTreeWithHistory {
         return _nextIndex;
     }
 
-    /**
-    @dev Whether the root is present in the root history
-  */
+    /// @dev Whether the root is present in the root history
     function isKnownRoot(bytes32 _root) public view returns (bool) {
         if (_root == 0) {
             return false;
@@ -128,9 +92,7 @@ contract MerkleTreeWithHistory {
         return false;
     }
 
-    /**
-    @dev Returns the last root
-  */
+    /// @dev Returns the last root
     function getLastRoot() public view returns (bytes32) {
         return roots[currentRootIndex];
     }
