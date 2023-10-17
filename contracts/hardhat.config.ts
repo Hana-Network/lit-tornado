7bfafcaf0421b251a328b34c537e6d92357290b0
Nobuhito Kurose <pudding.heart.hd@gmail.com>
2023-10-17 15:51:54 +0900

    add script for contract deploy

--- contracts/hardhat.config.ts
+++ contracts/hardhat.config.ts
@@ -1,8 +1,20 @@
+import * as dotenv from "dotenv";
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox-viem";
 
+dotenv.config();
+
 const config: HardhatUserConfig = {
   solidity: "0.8.19",
+  networks: {
+    mumbai: {
+      url: process.env.MUMBAI_RPC_ENDPOINT,
+      accounts: [process.env.PRIVATE_KEY!],
+    },
+  },
+  etherscan: {
+    apiKey: process.env.ETHERSCAN_API_KEY,
+  },
 };
 
 export default config;
--- contracts/scripts/deploy.ts
+++ contracts/scripts/deploy.ts
@@ -1,21 +1,20 @@
-import { formatEther, parseEther } from "viem";
+// npx hardhat run scripts/deploy.ts --network mumbai
+import { parseEther } from "viem";
 import hre from "hardhat";
 
+const VERIFIER_ADDRESS = "0x951444F56EF94FeC42e8cDBeDef1A4Dc1D1ea63B";
+const DENOMINATION = parseEther("0.1");
+const TREE_HEIGHT = 10;
+
 async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const unlockTime = BigInt(currentTimestampInSeconds + 60);
+  const litTornado = await hre.viem.deployContract("LitTornado", [
+    VERIFIER_ADDRESS,
+    DENOMINATION,
+    TREE_HEIGHT,
+  ]);
 
-  const lockedAmount = parseEther("0.001");
-
-  const lock = await hre.viem.deployContract("Lock", [unlockTime], {
-    value: lockedAmount,
-  });
-
-  console.log(
-    `Lock with ${formatEther(
-      lockedAmount
-    )}ETH and unlock timestamp ${unlockTime} deployed to ${lock.address}`
-  );
+  // 0xec3a882a8f786a34fd305a2c9b3a45b0c78e4a91
+  console.log("LitTornado deployed to:", litTornado.address);
 }
 
 // We recommend this pattern to be able to use async/await everywhere
