c4ed1844a65506c41f98a7f3e831ef66d4e15795
Nobuhito Kurose <pudding.heart.hd@gmail.com>
2023-10-20 15:53:34 +0900

    fix

--- contracts/test/utils.ts
+++ contracts/test/utils.ts
@@ -1,4 +1,5 @@
 import { keccak256, encodePacked } from "viem";
+import crypto from "crypto";
 
 const hashLeftRight = (left: `0x${string}`, right: `0x${string}`) => {
   return keccak256(encodePacked(["bytes32", "bytes32"], [left, right]));
@@ -8,7 +9,7 @@ export function verifyMerkleProof(
   leaf: `0x${string}`,
   root: `0x${string}`,
   pathElements: `0x${string}`[],
-  pathIndices: `0x${string}`[]
+  pathIndices: boolean[]
 ) {
   let computedHash = leaf;
 
@@ -23,3 +24,18 @@ export function verifyMerkleProof(
 
   return computedHash === root;
 }
+
+export function generateRandom32BytesHex(): `0x${string}` {
+  const randomBytes = crypto.getRandomValues(new Uint8Array(32));
+  const randomHex = Array.from(randomBytes)
+    .map((byte) => byte.toString(16).padStart(2, "0"))
+    .join("");
+  return `0x${randomHex}`;
+}
+
+export const generateCommitment = (
+  secret: `0x${string}`,
+  nullifier: `0x${string}`
+) => {
+  return keccak256(encodePacked(["bytes32", "bytes32"], [secret, nullifier]));
+};
